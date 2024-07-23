c8176ac3ffcd49ac746d001169a13db11016aa7b
Anabeatriz-Lang <oliveira2007araujo@gmail.com>
2024-07-23 16:24:43 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+print('ola mundo')
+
+numerointeiro = 42
+print( numerointeiro)
+
+numerofloat= 2.5
+
+print(numerofloat)
+
+string= "essa é uma variavel do tipo de dados string!"
+
+condicao = True
+nome= input("escreva seu nome: ")
+
+print(nome)
+
+idade= int(input("escreva a sua idade: "))
+print(idade)
+
+num1= float(input("escreva um numero:"))
+num2= float(input("escreva outro numero:"))
+
+soma= num1 + num2
+subtracao= num1 - num2
+multiplicacao= num1 * num2
+divisao= num1 / num2
+
+print(f"soma: {soma}")
+print(f"subtracao: {subtracao}")
+print(f"multiplicacao: {multiplicacao}")
+print(f"divisao: {divisao}")
